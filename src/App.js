54a263a527538c16c38a81bafb7953eeecadb228
FrankTimmons <franktimmonspdx@gmail.com>
2023-04-20 15:55:43 -0700

    Add Navbar and some styling for it.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <h1 className="text-2xl font-bold">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
