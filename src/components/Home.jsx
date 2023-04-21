6dbac45d7a92f17f8b038fb7c721900586d4bdad
FrankTimmons <franktimmonspdx@gmail.com>
2023-04-21 15:55:11 -0700

    Complete the MVP Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,9 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#124559]'>
       {/* Container */}
       <div className='max-w-[900px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#FF7373] font-semibold'>Howdy! I'm</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#EFF6E0]'>Frank Timmons</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#AEC3B0]'>Full Stack Web-Developer</h2> 
-        <p className='text-[#AEC3B0] py-4 max-w-[700px] font-semibold'>An aspiring developer transitioning from a career in art production and logistics. I love to problem solve and figure out what goes into making things work.  I thrive in a position where I can solve technical problems and make meaningful decisions related to optimization and user experience.  I want to make apps fun!</p>
+        <p className='text-[#AEC3B0] py-4 max-w-[700px] font-semibold'>I'm someone who loves figuring out how things work and finding ways to make them better. I used to work in art production and logistics, but now I'm working towards becoming a developer. I really enjoy technical roles where I can use my problem-solving skills to make apps that are fun and engaging for users.</p>
         <div>
           <button className='text-[#EFF6E0] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#FF7373] hover:border-[#FF7373] duration-200'>
             View Work 
