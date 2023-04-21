2f8848f51617a4896ddaa3098087ae78a5c9779b
FrankTimmons <franktimmonspdx@gmail.com>
2023-04-20 17:54:38 -0700

    Complete Navbar Component base functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,62 @@
-import React from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React, { useState } from 'react'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav);
+
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#01161E] text-[#F2E3DB]'>
       <div>
         <img src={Logo} alt="logo" style={{width:'50px'}}/>
       </div>
 
       {/* Menu */}
-      <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul className='hidden md:flex font-bold'>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>SKILLS</li>
+        <li>WORK</li>
+        <li>CONTACT</li>
       </ul>
 
       {/* Hamburger */}
-      <div className='md:hidden'>
-        <FaBars/>
+      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
+        {!nav ? <FaBars/> : <FaTimes/>}
       </div>
 
       {/* Mobile Menu */}
-      <ul className='md:hidden'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#01161E] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-4xl font-bold'>HOME</li>
+        <li className='py-6 text-4xl font-bold'>ABOUT</li>
+        <li className='py-6 text-4xl font-bold'>SKILLS</li>
+        <li className='py-6 text-4xl font-bold'>WORK</li>
+        <li className='py-6 text-4xl font-bold'>CONTACT</li>
       </ul>
+
+      {/* Social Icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1]'>
+            <a className='flex justify-between items-center w-full text-[#EFF6E0] font-bold' href="https://www.linkedin.com/in/frank-timmons-pdx/">
+              LINKEDIN <FaLinkedin size={30}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]'>
+            <a className='flex justify-between items-center w-full text-[#EFF6E0] font-bold' href="https://github.com/FrankTimmons">
+              GITHUB <FaGithub size={30}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#BB001B]'>
+            <a className='flex justify-between items-center w-full text-[#EFF6E0] font-bold' href="mailto: franktimmonspdx@gmail.com">
+              EMAIL <HiOutlineMail size={30}/>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
