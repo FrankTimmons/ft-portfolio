19e8a89b9f4c7d7f5a69e76d6a2cf140d3a108ce
FrankTimmons <franktimmonspdx@gmail.com>
2023-04-24 17:35:18 -0700

    Add contact component to app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <Skills />
       <Work />
+      <Contact />
     </div>
   );
 }
