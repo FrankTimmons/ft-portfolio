382bfe21d3a4d15c08bcbe341dd7142bd368ceba
FrankTimmons <franktimmonspdx@gmail.com>
2023-04-24 17:46:00 -0700

    Add react-scroll to the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo.png'
 
@@ -17,19 +18,27 @@ const Navbar = () => {
       {/* Menu */}
       <ul className='hidden md:flex font-bold'>
         <li className='group flex flex-col items-center hover:text-logo'>
-          HOME
+          <Link activeClass="active" to="home" smooth={true} duration={500}>
+            HOME
+          </Link>
           <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
         </li>
         <li className='group flex flex-col items-center hover:text-logo'>
-          SKILLS
+          <Link activeClass="active" to="skills" smooth={true} duration={500}>
+            SKILLS
+          </Link>
           <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
         </li>
         <li className='group flex flex-col items-center hover:text-logo'>
-          WORK
+          <Link activeClass="active" to="work" smooth={true} duration={500}>
+            WORK
+          </Link>
           <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
         </li>
         <li className='group flex flex-col items-center hover:text-logo'>
-          CONTACT
+          <Link activeClass="active" to="contact" smooth={true} duration={500}>
+            CONTACT
+          </Link>
           <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
         </li>
       </ul>
@@ -41,10 +50,26 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       <ul className={!nav ? 'hidden' : 'md:hidden absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>HOME</li>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>SKILLS</li>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>WORK</li>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>CONTACT</li>
+        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
+          <Link onClick={handleClick} activeClass="active" to="home" smooth={true} duration={500}>
+            HOME
+          </Link>
+        </li>
+        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
+          <Link onClick={handleClick} activeClass="active" to="skills" smooth={true} duration={500}>
+            SKILLS
+          </Link>
+        </li>
+        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
+          <Link onClick={handleClick} activeClass="active" to="work" smooth={true} duration={500}>
+            WORK
+          </Link>
+        </li>
+        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
+          <Link onClick={handleClick} activeClass="active" to="contact" smooth={true} duration={500}>
+            CONTACT
+          </Link>
+        </li>
       </ul>
 
       {/* Social Icons */}
