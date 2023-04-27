7117ce21ba5df9e1b9488167d45d858dcfda730e
FrankTimmons <franktimmonspdx@gmail.com>
2023-04-27 15:30:31 -0700

    Comment out incompleted work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,11 +8,12 @@ const Work = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-beige border-logo'>Work</p>
+          <p className='py-4 text-2xl'>Coming soon!</p>
         </div>
 
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
          
-          <div 
+          {/* <div 
             style={{ backgroundImage: `url(${GrindTime})` }} 
             className='shadow-lg shadow-primary group container rounded-md flex justify-center items-center mx-auto content-div'
           >
@@ -55,7 +56,7 @@ const Work = () => {
                 </a>
               </div>
             </div>
-          </div>          
+          </div>           */}
         </div>
       </div>
     </div>
