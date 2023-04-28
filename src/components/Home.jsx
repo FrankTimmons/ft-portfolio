0a8d323808e9f9a4d5163aa2c35e9534000d4ada
FrankTimmons <franktimmonspdx@gmail.com>
2023-04-27 18:46:22 -0700

    Fix react-scroll link tags to work on the entire element

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
-import { Link } from 'react-scroll'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-secondary'>
+    <div name="home" className="w-full h-screen bg-secondary">
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <h1 className='text-6xl sm:text-7xl font-bold text-beige'>Frank Timmons</h1>
-        <h2 className='text-6xl sm:text-7xl font-bold text-ash-gray'>Full Stack Web-Developer</h2> 
-        <p className='text-ash-gray py-4 max-w-[700px] font-semibold text-lg'>I'm someone who loves figuring out how things work and figuring out how to make them better. I used to work in art production and logistics, but now I'm working towards becoming a full-stack developer. I'm eager to be in a technical role where I can grow and use my problem-solving skills to make apps that are fun and engaging for everyone!</p>
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <h1 className="text-6xl sm:text-7xl font-bold text-beige">
+          Frank Timmons
+        </h1>
+        <h2 className="text-6xl sm:text-7xl font-bold text-ash-gray">
+          Full Stack Web-Developer
+        </h2>
+        <p className="text-ash-gray py-4 max-w-[700px] font-semibold text-lg">
+          I'm someone who loves figuring out how things work and figuring out
+          how to make them better. I used to work in art production and
+          logistics, but now I'm working towards becoming a full-stack
+          developer. I'm eager to be in a technical role where I can grow and
+          use my problem-solving skills to make apps that are fun and engaging
+          for everyone!
+        </p>
         <div>
-          <button className='text-beige group border-2 px-6 py-3 my-2 flex items-center hover:bg-logo hover:border-logo duration-200'>
-            <Link activeClass="active" to="work" smooth={true} duration={500}>
+          <Link
+            className="block"
+            activeClass="active"
+            to="work"
+            smooth={true}
+            duration={500}
+          >
+            <button className="text-beige group border-2 px-6 py-3 my-2 flex items-center hover:bg-logo hover:border-logo duration-200">
               View Work
-            </Link> 
-            <span className='group-hover:rotate-90 origin-[70%_50%] duration-100'>
-              <HiArrowNarrowRight className='ml-3'/>
-            </span>
-          </button>
+              <span className="group-hover:rotate-90 origin-[70%_50%] duration-100">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,104 +1,174 @@
-import React, { useState } from 'react'
-import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import { Link } from 'react-scroll'
-import { BsFillPersonLinesFill } from 'react-icons/bs'
-import Logo from '../assets/logo.png'
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { Link } from "react-scroll";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-primary text-beige z-20'>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-primary text-beige z-20">
       <div>
-        <img src={Logo} alt="logo" style={{width:'50px'}}/>
+        <img src={Logo} alt="logo" style={{ width: "50px" }} />
       </div>
 
       {/* Menu */}
-      <ul className='hidden md:flex font-bold'>
-        <li className='group flex flex-col items-center hover:text-logo'>
-          <Link activeClass="active" to="home" smooth={true} duration={500}>
+      <ul className="hidden md:flex font-bold">
+        <Link
+          className="block"
+          activeClass="active"
+          to="home"
+          smooth={true}
+          duration={500}
+        >
+          <li className="group flex flex-col items-center hover:text-logo">
             HOME
-          </Link>
-          <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
-        </li>
-        <li className='group flex flex-col items-center hover:text-logo'>
-          <Link activeClass="active" to="skills" smooth={true} duration={500}>
+            <div className="bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1"></div>
+          </li>
+        </Link>
+        <Link
+          className="block"
+          activeClass="active"
+          to="skills"
+          smooth={true}
+          duration={500}
+        >
+          <li className="group flex flex-col items-center hover:text-logo">
             SKILLS
-          </Link>
-          <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
-        </li>
-        <li className='group flex flex-col items-center hover:text-logo'>
-          <Link activeClass="active" to="work" smooth={true} duration={500}>
+            <div className="bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1"></div>
+          </li>
+        </Link>
+        <Link
+          className="block"
+          activeClass="active"
+          to="work"
+          smooth={true}
+          duration={500}
+        >
+          <li className="group flex flex-col items-center hover:text-logo">
             WORK
-          </Link>
-          <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
-        </li>
-        <li className='group flex flex-col items-center hover:text-logo'>
-          <Link activeClass="active" to="contact" smooth={true} duration={500}>
+            <div className="bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1"></div>
+          </li>
+        </Link>
+        <Link
+          className="block"
+          activeClass="active"
+          to="contact"
+          smooth={true}
+          duration={500}
+        >
+          <li className="group flex flex-col items-center hover:text-logo">
             CONTACT
-          </Link>
-          <div className='bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1'></div>
-        </li>
+            <div className="bg-primary group-hover:bg-logo flex w-[8px] h-[8px] rounded-[4px] duration-200 mt-1"></div>
+          </li>
+        </Link>
       </ul>
 
       {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
-        {!nav ? <FaBars/> : <FaTimes/>}
+      <div onClick={handleClick} className="md:hidden z-10 cursor-pointer">
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile Menu */}
-      <ul className={!nav ? 'hidden' : 'md:hidden absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
-          <Link onClick={handleClick} activeClass="active" to="home" smooth={true} duration={500}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "md:hidden absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl font-bold hover:text-logo duration-200">
+          <Link
+            onClick={handleClick}
+            activeClass="active"
+            to="home"
+            smooth={true}
+            duration={500}
+          >
             HOME
           </Link>
         </li>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
-          <Link onClick={handleClick} activeClass="active" to="skills" smooth={true} duration={500}>
+        <li className="py-6 text-4xl font-bold hover:text-logo duration-200">
+          <Link
+            onClick={handleClick}
+            activeClass="active"
+            to="skills"
+            smooth={true}
+            duration={500}
+          >
             SKILLS
           </Link>
         </li>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
-          <Link onClick={handleClick} activeClass="active" to="work" smooth={true} duration={500}>
+        <li className="py-6 text-4xl font-bold hover:text-logo duration-200">
+          <Link
+            onClick={handleClick}
+            activeClass="active"
+            to="work"
+            smooth={true}
+            duration={500}
+          >
             WORK
           </Link>
         </li>
-        <li className='py-6 text-4xl font-bold hover:text-logo duration-200'>
-          <Link onClick={handleClick} activeClass="active" to="contact" smooth={true} duration={500}>
+        <li className="py-6 text-4xl font-bold hover:text-logo duration-200">
+          <Link
+            onClick={handleClick}
+            activeClass="active"
+            to="contact"
+            smooth={true}
+            duration={500}
+          >
             CONTACT
           </Link>
         </li>
       </ul>
 
       {/* Social Icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1]'>
-            <a target='blank' className='flex justify-between items-center w-full text-beige font-bold' href="https://www.linkedin.com/in/frank-timmons-pdx/">
-              LINKEDIN <FaLinkedin size={30}/>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0072b1]">
+            <a
+              target="blank"
+              className="flex justify-between items-center w-full text-beige font-bold"
+              href="https://www.linkedin.com/in/frank-timmons-pdx/"
+            >
+              LINKEDIN <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]'>
-            <a target='blank' className='flex justify-between items-center w-full text-beige font-bold' href="https://github.com/FrankTimmons">
-              GITHUB <FaGithub size={30}/>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]">
+            <a
+              target="blank"
+              className="flex justify-between items-center w-full text-beige font-bold"
+              href="https://github.com/FrankTimmons"
+            >
+              GITHUB <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#BB001B]'>
-            <a target='blank' className='flex justify-between items-center w-full text-beige font-bold' href="mailto: franktimmonspdx@gmail.com">
-              EMAIL <HiOutlineMail size={30}/>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#BB001B]">
+            <a
+              target="blank"
+              className="flex justify-between items-center w-full text-beige font-bold"
+              href="mailto: franktimmonspdx@gmail.com"
+            >
+              EMAIL <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0da36a]'>
-            <a target='blank' className='flex justify-between items-center w-full text-beige font-bold' href="https://www.canva.com/design/DAFM5OT8Yfw/x_4Hh42MxGszfokSNnuMLw/view?utm_content=DAFM5OT8Yfw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink">
-              RESUME <BsFillPersonLinesFill size={30}/>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0da36a]">
+            <a
+              target="blank"
+              className="flex justify-between items-center w-full text-beige font-bold"
+              href="https://www.canva.com/design/DAFM5OT8Yfw/x_4Hh42MxGszfokSNnuMLw/view?utm_content=DAFM5OT8Yfw&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+            >
+              RESUME <BsFillPersonLinesFill size={30} />
             </a>
           </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
